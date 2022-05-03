dcbd7bb5b9a54303f78e22710ed5b6bc231c6e9a
Jean-Paul <a21jeaha@student.his.se>
2022-05-03 11:50:55 +0200

    EditText widget added to activity_second.xml

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,13 +2,21 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private SharedPreferences mySharedPreferenceRef;                    // shared preference object skaoad
+    private SharedPreferences.Editor mySharedPreferenceEditor;          //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        mySharedPreferenceRef = getPreferences(MODE_PRIVATE);           // shared preference initierad
+        mySharedPreferenceEditor = mySharedPreferenceRef.edit();        //
+
     }
 }
