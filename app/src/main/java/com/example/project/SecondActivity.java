6ef31500f591f9f32b91cd7033e0d0288321d936
Jean-Paul <a21jeaha@student.his.se>
2022-05-03 17:58:10 +0200

    added a clicklistener to acceptTextButton, preference implemented initiated in onTappSecond()

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
-    private TextView textView;
-    private EditText editText;
+    private TextView textView;                                          //
+    private EditText editText;                                          // diverse widgets skapas
+    private Button acceptTextButton;                                    //
 
     private SharedPreferences mySharedPreferenceRef;                    // shared preference object skaoad
     private SharedPreferences.Editor mySharedPreferenceEditor;          //
@@ -20,12 +23,28 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        acceptTextButton = findViewById(R.id.accept_text_Button);               // knappen kommer användas för att registrera text som skrivits i editText widgeten.
+        acceptTextButton.setOnClickListener(new View.OnClickListener() {        //
+            @Override
+            public void onClick(View v) {
+                onTappSecond();
+            }
+        });
+
         textView = findViewById(R.id.second_activity_text);
         editText = findViewById(R.id.test_edit_demo);
-        textView.setText(editText.getText().toString());      ////// FINISH UPP THIS PROCESS
+
 
         mySharedPreferenceRef = getPreferences(MODE_PRIVATE);           // shared preference initierad
         mySharedPreferenceEditor = mySharedPreferenceRef.edit();        //
 
+        textView.setText(mySharedPreferenceRef.getString("text_from_edittext","nothing written here.."));
+
+    }
+
+    private void onTappSecond(){
+        textView.setText(editText.getText().toString());         // det som står skrivet i EditText widgeten skickas till textView och vissas upp
+        mySharedPreferenceEditor.putString("text_from_edittext", textView.getText().toString());    // placerar String-värdert i en preference
+        mySharedPreferenceEditor.apply();                                                           //
     }
 }
